1a3f7c8b1247c37b598db59493686925496afb41
Sascha <k.puligheddu@alice.it>
2026-03-09 19:51:01 +0100

    Update BufferPool.cs
    missing namespace (copy-paste error)

--- Server/Network/BufferPool.cs
+++ Server/Network/BufferPool.cs
@@ -22,71 +22,74 @@ using System.Collections.Generic;
 using System.Collections.Concurrent;
 using System.Threading;
 
-public class BufferPool
+namespace Server.Network
 {
-    private static readonly List<BufferPool> m_Pools = new List<BufferPool>();
-    public static List<BufferPool> Pools => m_Pools;
-
-    private readonly ConcurrentStack<byte[]> m_FreeBuffers = new();
-
-    private readonly string m_Name;
-    private readonly int m_InitialCapacity;
-    private readonly int m_BufferSize;
-    private int m_Misses;
-    private int m_CurrentCreated; // Tracking of created arrays
-
-    public BufferPool(string name, int initialCapacity, int bufferSize)
+    public class BufferPool
     {
-        m_Name = name;
-        m_InitialCapacity = initialCapacity;
-        m_BufferSize = bufferSize;
-        m_CurrentCreated = initialCapacity;
+        private static readonly List<BufferPool> m_Pools = new List<BufferPool>();
+        public static List<BufferPool> Pools => m_Pools;
 
-        // Pre-allocate valid contiguous memory array: One single block to avoid LOH
-        for (int i = 0; i < initialCapacity; ++i)
+        private readonly ConcurrentStack<byte[]> m_FreeBuffers = new();
+
+        private readonly string m_Name;
+        private readonly int m_InitialCapacity;
+        private readonly int m_BufferSize;
+        private int m_Misses;
+        private int m_CurrentCreated; // Tracking of created arrays
+
+        public BufferPool(string name, int initialCapacity, int bufferSize)
         {
-            m_FreeBuffers.Push(new byte[bufferSize]);
+            m_Name = name;
+            m_InitialCapacity = initialCapacity;
+            m_BufferSize = bufferSize;
+            m_CurrentCreated = initialCapacity;
+
+            // Pre-allocate valid contiguous memory array: One single block to avoid LOH
+            for (int i = 0; i < initialCapacity; ++i)
+            {
+                m_FreeBuffers.Push(new byte[bufferSize]);
+            }
+
+            lock (m_Pools) { m_Pools.Add(this); }
         }
 
-        lock (m_Pools) { m_Pools.Add(this); }
-    }
-
-    public byte[] AcquireBuffer()
-    {
-        if (m_FreeBuffers.TryPop(out byte[] buffer))
+        public byte[] AcquireBuffer()
         {
-            return buffer;
+            if (m_FreeBuffers.TryPop(out byte[] buffer))
+            {
+                return buffer;
+            }
+
+            // Increment misses and create a new buffer
+            Interlocked.Increment(ref m_Misses);
+            Interlocked.Increment(ref m_CurrentCreated);
+
+            return new byte[m_BufferSize];
         }
 
-        // Increment misses and create a new buffer
-        Interlocked.Increment(ref m_Misses);
-        Interlocked.Increment(ref m_CurrentCreated);
+        public void ReleaseBuffer(byte[] buffer)
+        {
+            // Don't allow null or external buffers here
+            if (buffer == null || buffer.Length != m_BufferSize)
+                return;
 
-        return new byte[m_BufferSize];
-    }
+            m_FreeBuffers.Push(buffer);
+        }
 
-    public void ReleaseBuffer(byte[] buffer)
-    {
-        // Don't allow null or external buffers here
-        if (buffer == null || buffer.Length != m_BufferSize)
-            return;
+        public void GetInfo(out string name, out int freeCount, out int initialCapacity, out int currentCapacity, out int bufferSize, out int misses)
+        {
+            name = m_Name;
+            freeCount = m_FreeBuffers.Count;
+            initialCapacity = m_InitialCapacity;
+            currentCapacity = Volatile.Read(ref m_CurrentCreated);
+            bufferSize = m_BufferSize;
+            misses = Volatile.Read(ref m_Misses);
+        }
 
-        m_FreeBuffers.Push(buffer);
-    }
-
-    public void GetInfo(out string name, out int freeCount, out int initialCapacity, out int currentCapacity, out int bufferSize, out int misses)
-    {
-        name = m_Name;
-        freeCount = m_FreeBuffers.Count;
-        initialCapacity = m_InitialCapacity;
-        currentCapacity = Volatile.Read(ref m_CurrentCreated);
-        bufferSize = m_BufferSize;
-        misses = Volatile.Read(ref m_Misses);
-    }
-
-    public void Free()
-    {
-        lock (m_Pools) { m_Pools.Remove(this); }
-        m_FreeBuffers.Clear();
+        public void Free()
+        {
+            lock (m_Pools) { m_Pools.Remove(this); }
+            m_FreeBuffers.Clear();
+        }
     }
 }
